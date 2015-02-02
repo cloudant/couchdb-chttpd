200964963e7dc1cc9726f16d120e10ad019572f2
Mike Wallace <mikewallace@apache.org>
2015-02-02 15:35:39 +0000

    Default name-munging prefix
    This commit adds a default (reserved) namespace which will be used
    if nothing is provided in the X-Couch-User header or hostname.
    
    The main benefit of this is that we no longer end up with shard
    maps that have illegal document IDs for the _users or _replicator
    DBs.
    
    BugzID: 43618

--- src/chttpd_util.erl
+++ src/chttpd_util.erl
@@ -1,6 +1,8 @@
 -module(chttpd_util).
 -export([customer_name/1, customer_db_info/2, customer_path/1]).
 
+-define(RESERVED_NAMESPACE, "default").
+
 customer_db_info(Req, Info) ->
     FullName = couch_util:get_value(db_name, Info),
     Customer = customer_path(Req),
@@ -21,13 +23,7 @@ customer_path(Req) ->
 
 %% internal
 
-customer_from_host_header(undefined, _DomainStr) ->
-    "";
-customer_from_host_header("", _DomainStr) ->
-    "";
-customer_from_host_header(_Host, "") ->
-    "";
-customer_from_host_header(Host, DomainStr) ->
+customer_from_host_header(Host, DomainStr) when is_list(Host), is_list(DomainStr), length(Host) > 0, length(DomainStr) > 0 ->
     Domain = lists:reverse(string:tokens(DomainStr, ".")),
     [Hostname|_Port] = string:tokens(Host, ":"),
     Hostname1 = lists:reverse(string:tokens(Hostname, ".")),
@@ -36,5 +32,7 @@ customer_from_host_header(Host, DomainStr) ->
         Rest = lists:nthtail(length(Domain), Hostname1),
         string:join(lists:reverse(Rest), ".");
     _ ->
-        ""
-    end.
+        config:get("chttpd", "reserved_namespace", ?RESERVED_NAMESPACE)
+    end;
+customer_from_host_header(_Host, _DomainStr) ->
+    config:get("chttpd", "reserved_namespace", ?RESERVED_NAMESPACE).
