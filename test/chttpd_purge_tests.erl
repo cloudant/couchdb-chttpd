8b636a821d42bb1cf5344679b122dbba6c897ab5
Mayya Sharipova <mayyas@ca.ibm.com>
2017-02-28 19:01:09 -0500

    Cluster Purge Improvements
    Remove test for purged_docs_limit
    as the behaviour changed
    
    BugzID: 68279

--- test/chttpd_purge_tests.erl
+++ test/chttpd_purge_tests.erl
@@ -56,7 +56,6 @@ purge_test_() ->
                 fun setup/0, fun teardown/1,
                 [
                     fun test_ok_purge_request/1,
-                    fun test_error_purge_request/1,
                     fun should_error_set_purged_docs_limit_to0/1
                 ]
             }
@@ -96,56 +95,6 @@ test_ok_purge_request(Url) ->
     end).
 
 
-test_error_purge_request(Url) ->
-    ?_test(begin
-        % set purged_docs_limit to 1
-        PDocsLimit = 1,
-        {ok, Status, _, _} = test_request:put(Url ++ "/_purged_docs_limit/",
-           [?CONTENT_JSON, ?AUTH], integer_to_list(PDocsLimit)),
-        ?assert(Status =:= 200),
-
-        % test db purged_docs_limit is set to PDocsLimit
-        {ok, Status2, _, ResultBody2} = test_request:get(Url ++
-            "/_purged_docs_limit/", [?AUTH]),
-        ?assert(Status2 =:= 200),
-        ResultJson2 = ?JSON_DECODE(ResultBody2),
-        ?assertEqual(PDocsLimit, ResultJson2),
-
-        % doc1 and doc5 fall on the same shard,
-        % and exceed purged_docs_limit for this shard
-        {ok,_,_,Body} = create_doc(Url, "doc1"), {Json} = ?JSON_DECODE(Body),
-        Rev1 = couch_util:get_value(<<"rev">>, Json),
-        {ok,_,_,Body2} = create_doc(Url, "doc2"), {Json2} = ?JSON_DECODE(Body2),
-        Rev2 = couch_util:get_value(<<"rev">>, Json2),
-        {ok,_,_,Body3} = create_doc(Url, "doc3"), {Json3} = ?JSON_DECODE(Body3),
-        Rev3 = couch_util:get_value(<<"rev">>, Json3),
-        {ok,_,_,Body4} = create_doc(Url, "doc4"), {Json4} = ?JSON_DECODE(Body4),
-        Rev4 = couch_util:get_value(<<"rev">>, Json4),
-        {ok,_,_,Body5} = create_doc(Url, "doc5"), {Json5} = ?JSON_DECODE(Body5),
-        Rev5 = couch_util:get_value(<<"rev">>, Json5),
-        IdsRevs = "{\"doc1\": [\"" ++ ?b2l(Rev1) ++ "\"], \"doc2\": [\"" ++
-            ?b2l(Rev2) ++ "\"], \"doc3\": [\"" ++ ?b2l(Rev3) ++
-            "\"], \"doc4\": [\"" ++ ?b2l(Rev4) ++ "\"], \"doc5\": [\"" ++
-            ?b2l(Rev5) ++ "\"] }",
-
-        {ok, Status3, _, ResultBody3} = test_request:post(Url ++ "/_purge/",
-            [?CONTENT_JSON, ?AUTH], IdsRevs),
-        ?assert(Status3 =:= 201 orelse Status3 =:= 202),
-        ResultJson3 = ?JSON_DECODE(ResultBody3),
-        ?assertMatch(
-            {[
-                {<<"purged">>, [
-                    {[{<<"id">>,<<"doc1">>}, {<<"error">>,<<"error">>}, {<<"reason">>,<<"purged_docs_limit_exceeded">>}]},
-                    {[{<<"ok">>,true}, {<<"id">>,<<"doc2">>}, {<<"revs">>,[Rev2]}]},
-                    {[{<<"ok">>,true}, {<<"id">>,<<"doc3">>}, {<<"revs">>,[Rev3]}]},
-                    {[{<<"ok">>,true}, {<<"id">>,<<"doc4">>}, {<<"revs">>,[Rev4]}]},
-                    {[{<<"id">>,<<"doc5">>}, {<<"error">>,<<"error">>}, {<<"reason">>,<<"purged_docs_limit_exceeded">>}]}
-                ]},
-                {<<"purge_seq">>, _}
-            ]},
-            ResultJson3
-        )
-    end).
 
 
 should_error_set_purged_docs_limit_to0(Url) ->
